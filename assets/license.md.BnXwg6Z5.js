e88e7468b8acbcae42733478245d62329e6e300a
neuxdotdev <neuxdev1@gmail.com>
2026-04-15 20:45:41 +0700

    Updates

---
+++ assets/license.md.BnXwg6Z5.js
@@ -0,0 +1,17 @@
+import{_ as n,o as a,c as e,ag as p}from"./chunks/framework.DXGyWiRo.js";const d=JSON.parse('{"title":"LICENSE","description":"","frontmatter":{},"headers":[],"relativePath":"license.md","filePath":"license.md","lastUpdated":1776259841000}'),i={name:"license.md"};function l(t,s,r,o,c,b){return a(),e("div",null,[...s[0]||(s[0]=[p(`<h1 id="license" tabindex="-1">LICENSE <a class="header-anchor" href="#license" aria-label="Permalink to &quot;LICENSE&quot;">​</a></h1><div class="language-txt vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">txt</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>MIT License</span></span>
+<span class="line"><span>Copyright (c) 2026 neuxdotdev</span></span>
+<span class="line"><span>Permission is hereby granted, free of charge, to any person obtaining a copy</span></span>
+<span class="line"><span>of this software and associated documentation files (the &quot;Software&quot;), to deal</span></span>
+<span class="line"><span>in the Software without restriction, including without limitation the rights</span></span>
+<span class="line"><span>to use, copy, modify, merge, publish, distribute, sublicense, and/or sell</span></span>
+<span class="line"><span>copies of the Software, and to permit persons to whom the Software is</span></span>
+<span class="line"><span>furnished to do so, subject to the following conditions:</span></span>
+<span class="line"><span>The above copyright notice and this permission notice shall be included in all</span></span>
+<span class="line"><span>copies or substantial portions of the Software.</span></span>
+<span class="line"><span>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span>IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span>FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span>AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span>LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,</span></span>
+<span class="line"><span>OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE</span></span>
+<span class="line"><span>SOFTWARE.</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br></div></div>`,2)])])}const T=n(i,[["render",l]]);export{d as __pageData,T as default};
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,108 @@
+import typedocSidebar from '../api/typedoc-sidebar.json'
+export default {
+	title: 'ApIgsReport Library',
+	description: 'Pure JavaScript library to automate SSRS reports with NTLM authentication',
+	lang: 'en-US',
+	ignoreDeadLinks: true,
+	cleanUrls: true,
+	head: [
+		['link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }],
+		['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+		['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
+		['link', { rel: 'manifest', href: '/site.webmanifest' }],
+		['meta', { name: 'theme-color', content: '#3eaf7c' }],
+		['meta', { name: 'og:type', content: 'website' }],
+		['meta', { name: 'og:title', content: 'ApIgsReport Library' }],
+		[
+			'meta',
+			{
+				name: 'og:description',
+				content: 'Pure TypeScript library for SSRS API automation with NTLM authentication',
+			},
+		],
+		['meta', { name: 'og:image', content: '/og-image.png' }],
+		['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+	],
+	markdown: {
+		lineNumbers: true,
+	},
+	vite: {
+		server: {
+			fs: {
+				allow: ['..'],
+			},
+		},
+	},
+	themeConfig: {
+		logo: '/logo.svg',
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'API Reference', link: '/api/globals' },
+			{ text: 'GitHub', link: 'https://github.com/neuxdotdev/apigsreport' },
+			{ text: 'npm', link: 'https://www.npmjs.com/package/apigsreport' },
+		],
+		sidebar: {
+			'/api/': [
+				{
+					text: 'API Reference',
+					items: typedocSidebar,
+				},
+			],
+			'/': [
+				{
+					text: 'Guide',
+					items: [
+						{ text: 'Home', link: '/' },
+						{ text: 'License', link: '/license' },
+					],
+				},
+			],
+		},
+		footer: {
+			message: 'Released under MIT License',
+			copyright: 'Copyright © 2026 neuxdotdev',
+		},
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/neuxdotdev/apigsreport' },
+			{ icon: 'npm', link: 'https://www.npmjs.com/package/apigsreport' },
+		],
+		editLink: {
+			pattern: 'https://github.com/neuxdotdev/apigsreport/edit/master/docs/:path',
+			text: 'Edit this page on GitHub',
+		},
+		lastUpdated: {
+			text: 'Last updated',
+			formatOptions: {
+				dateStyle: 'full',
+				timeStyle: 'medium',
+			},
+		},
+		search: {
+			provider: 'local',
+			options: {
+				translations: {
+					button: {
+						buttonText: 'Search',
+						buttonAriaLabel: 'Search docs',
+					},
+					modal: {
+						noResultsText: 'No results found',
+						resetButtonTitle: 'Reset search',
+						footer: {
+							selectText: 'to select',
+							navigateText: 'to navigate',
+						},
+					},
+				},
+			},
+		},
+		outline: {
+			level: 'deep',
+			label: 'On this page',
+		},
+		docFooter: {
+			prev: 'Previous page',
+			next: 'Next page',
+		},
+	},
+}
